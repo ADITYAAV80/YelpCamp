32236e2e1dce577d41dbff37a59db5f540c21d4d
ADITYAAV80 <adityaav80@gmail.com>
2025-01-16 13:39:00 +0530

    passing form data of registered user to backend

--- routes/user.js
+++ routes/user.js
@@ -10,8 +10,21 @@ routes.get("/register", (req, res) => {
   res.render("users/register", { title: "register" });
 });
 
-routes.post("/register", (req, res) => {
-  res.send(req.body);
-});
+routes.post(
+  "/register",
+  catchAsync(async (req, res) => {
+    try {
+      const { email, username, password } = req.body;
+      const user = new User({ email, username });
+      const registeredUser = await User.register(user, password);
+      console.log(registeredUser);
+      req.flash("success", "Welcome to Yelp Camp!!");
+      res.redirect("/campgrounds");
+    } catch (e) {
+      req.flash("error", e.message);
+      res.redirect("/users/register");
+    }
+  })
+);
 
 module.exports = routes;
