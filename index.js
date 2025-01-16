f1fbed83f90f107b22c08cbc0a05ae071db5d8a6
ADITYAAV80 <adityaav80@gmail.com>
2025-01-16 19:35:05 +0530

    adding newly registered user to session

--- index.js
+++ index.js
@@ -71,8 +71,6 @@ app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   res.locals.currentUser = req.user;
-
-  console.log(req.user);
   next();
 });
 
